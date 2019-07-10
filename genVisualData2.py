e4eeae2ee707d4e9f8cf92fb559d74dc2307b56f
Mike Flanagan <mflan48@gmail.com>
2019-07-10 14:52:11 -0400

    Fixed combined df, need to add command line functionality and then afix the code for the createRadial,alpscarf, and scatter functions

--- genVisualData2.py
+++ genVisualData2.py
@@ -44,58 +44,58 @@ def convertAOI(myStrX):
 def convertColor(myColor):
     return ( ("\"" + "#"+str(myColor) + "\"" ) if myColor[0] != '#' else ("\"" + str(myColor) + "\"") )
 
-"""
-    Gets the amount of AOI's in a passed in jsonFile
-
-    Requirements:
-    pathName is a valid path and does not include the ending /
-
-    Parameters:
-    pathName: string :path to where the jsonFile is
-    jsonFileName: string :the name of the json file to read
-
-    Return:
-    Length of the json file which corresponds to how many AOI's are in that file (including the -1 AOI which is either whitespace or not an AOI)    
-"""
-def getAmountAOI(pathName,jsonFileName):
-    toOpen = pathName  + "/" + jsonFileName
-    with open(toOpen) as jsonFile:
+#jsonFilePath is the full path to the jsonFile
+def getAmountAOI(jsonFilePath):
+    with open(jsonFilePath,"r") as jsonFile:
         return(len(json.load(jsonFile)))
+
+#THE pathToPost MUST NOT CONTAIN THE FINAL /. FILTER THIS OUT IN MAIN
+#globQuery is what we want to search for in the directory. for csv use /*.csv and for json use /*.json
+def getFilePaths(pathToPost,globQuery):
+    
+    #Check if that phase/post2aoi path exist
+    if(not os.path.exists(pathToPost)):
+        print("Error: Could not find path " + pathToPost)
+        exit(1)
+
+    pathFiles = pathToPost + globQuery
+    files = glob.glob(pathFiles)
+
+    """
+    for i in range(0,len(files)):
+        files[i] = os.path.split(files[i])[1]
+    """
+    return(sorted(files))
+
+
+#JUST GET THE ORIGNAL MINESWEEPER FILE NAME AND ADD .java
+#how we store the 
+def getFileNameFromPath(filePath):
+    #split path to just get the file then just get the file.java
+    return(os.path.split(filePath)[1].split(".")[0] + ".java")
+        #fileName.split(".")[0] + ".java")
+
 
 """
     Searches a given directory for all json files and returns a dictionary with following key value pair
-    {fileName : amount of AOI's}
-
-    Parameters:
-    pathName: string :the path to search for .json files (these files are the ones generated from Ian's AOI tool)
+    {minesweeperFileName : amount of AOI's}
 
     Return:
-    A dictionary of {fileName : amountAOI's in file}
+    A dictionary of {minesweeperFileName : amountAOI's in file}
 """
-def getAllAOISize(pathName):
-    #Check if the path exist
-    if(not os.path.exists(pathName)):
-        print("Error: Could not find path" + pathName)
-        exit(1)
-
-    #use glob module to find all of the json files in the path
-    myPath = pathName + "/*.json"
-    files = glob.glob(myPath)
+def getAllAOISize(listJSONPath):
 
     #If there are no files, throw error
-    if(len(files) == 0):
-        print("Error: No json files found in path " + pathName)
+    if(len(listJSONPath) == 0):
+        print("Error: No Json files exist")
         exit(1)
     
-
     toReturn = {}
     #Go thru each of the files and get the length of the AOI's in them 
-    for i in files:
-        #Split the path and get the file
-        temp = os.path.split(i)
-        
-        #get the file name without the .json extension
-        toReturn[os.path.splitext(temp[1])[0]] = getAmountAOI(temp[0], temp[1])
+    for curPath in listJSONPath:
+        #get the file name without the .json extension and just the original file.java
+        #toReturn[getCodeFile(curFile)] = getAmountAOI(pathToPost, curFile)
+        toReturn[getFileNameFromPath(curPath)] = getAmountAOI(curPath)
     return toReturn        
 
 """
@@ -109,40 +109,30 @@ def getAllAOISize(pathName):
     A dictionary with key value pair {fileName: numberOffSet for AOI}
     Also prints the numbering of AOI's for each file 
 """
-def getNumToAdjustAOI(jsonPath, csvPath):
+def getNumToAdjustAOI(listJSONPath,listCSVPath):
 
     #Create the dictionary of AOI sizes
-    jsonDict = getAllAOISize(jsonPath)
+    jsonDict = getAllAOISize(listJSONPath)
 
-    #find all the csv's in the csvPath
-    csvPathName = csvPath + "/*.csv" 
-    csvFiles = glob.glob(csvPathName)
-    if(len(csvFiles) == 0):
-        print("Error: No files exist")
-        exit(1)
-    elif(len(jsonDict) != len(csvFiles)):
-        print("Error: there are different amount of .csv files and .json files")
-        exit(1)
 
     #keeps track of the offset AOI number
     curOffSet = 0
 
     toReturn = {}
 
-    for i in csvFiles:
+    for i in listCSVPath:
         #get the the .csv fileName
-        splittedPath = os.path.splitext(os.path.split(i)[1])[0]
+        curCSVName = getFileNameFromPath(i)
 
         #Check to see if it exist in the jsonDict. If it does then print the numbers for which it AOIS will be globally
         # If its not, that means either the .csv doesnt exist or their was not a jsonFile for it 
-        if(splittedPath in jsonDict.keys()):
-            print(splittedPath + " AOI's will be number " + str(curOffSet) + " to " + str(curOffSet + jsonDict[splittedPath]-1) + "\n")
-            toReturn[splittedPath] = curOffSet
-            curOffSet = curOffSet + jsonDict[splittedPath]
+        if(curCSVName in jsonDict.keys()):
+            print(curCSVName + " AOI's will be number " + str(curOffSet) + " to " + str(curOffSet + jsonDict[curCSVName]-1) + "\n")
+            toReturn[curCSVName] = curOffSet
+            curOffSet = curOffSet + jsonDict[curCSVName]
         else:
-            print("Error: " + splittedPath + "either does not have a json file or does not exist ")
+            print("Error: " + curCSVName + "either does not have a json file or does not exist ")
             exit(1)
-
     return(toReturn)
 
 """
@@ -166,74 +156,31 @@ def findEarliestTime(dataFramesDict):
             earliestTime = curTime
     return(earliestTime)
 
-"""
-    Creates the colorMatching dictionary based off an input file
-
-    Requirments:
-    Each line of Match file is of the following format
-    Color1,<...>        <...> means the list of AOI's with this color each seperated by commas.  Ex: 1,3,5,45,69
-
-    Parameters:
-    matchFile: string :the name of the file to read from
-
-    Return:
-    Dictionary with key value pair {hex color: list AOIs with that color}
-"""
-def createColorMatching(matchFile):
-
-    toReturn = {}
-    try:
-        with open(matchFile, "r") as colorFile:
-            contents = [x.strip("\n") for x in colorFile.readlines()]
-            for line in contents:
-                stripped = line.replace(" ","").split(",")
-                color = stripped[0]
-                AOIs = stripped[1:]
-                if(not color in toReturn.keys()):
-                    toReturn[color] = AOIs
-                else:
-                    print("Error: Multiple colors exist in" + matchFile)
-                    exit(1)
-    except FileNotFoundError:
-        print("Error: " + matchFile + " could not be found")
-        exit(1)
-
-    return(toReturn)
 
 """
     Read each of the iTrace-post files and creates on big pandas data frame 
-
-    Parameters:
-    jsonPath: string :path to directry with jsonFiles
-    csvPath: string :path to directory with csvFiles
 
     Returns:
     A pandas dataframe in time order of the following format
     (['fix_col', 'fix_line', 'fix_time', 'fix_dur' (milliseconds), 'which_file', 'AOI'])
 """
-def createCombinedDF(jsonPath, csvPath):
-        
+def createCombinedDF(listJSONPath,listCSVPath): 
     #get the amount to adjust each AOI
-    numAdjust = getNumToAdjustAOI(jsonPath,csvPath)
+    numAdjust = getNumToAdjustAOI(listJSONPath,listCSVPath)
 
     #Dictionary used to store all dataframes
     allFrames = {}
 
-    #find all the csv's in the csvPath
-    csvPathName = csvPath + "/*.csv" 
-    csvFiles = glob.glob(csvPathName)
-    
     #Read in and create all of the dataFrames, edit their AOI's
-    for i in csvFiles:
+    for curPath in listCSVPath:
         #Get the name of the file
-        curName = os.path.splitext(os.path.split(i)[1])[0]
+        curName = getFileNameFromPath(curPath)
         
         #Read it in
-        allFrames[curName] = pd.read_csv(i)
+        allFrames[curName] = pd.read_csv(curPath)
         #Update the AOI's
         allFrames[curName].AOI = np.where(allFrames[curName].AOI >= 0, allFrames[curName].AOI + numAdjust[curName] , -1)
     
-
     #find the earliest time
     earliestTime = findEarliestTime(allFrames)
 
@@ -258,11 +205,11 @@ def createCombinedDF(jsonPath, csvPath):
     Return:
     Creates csv called "scatterData.csv"
 """
-def createScatter(combinedDF, partName = "P_default"):
+def createScatter(combinedDF, partName = "P0"):
     #only get the fix_time and AOI
     scatterDF = combinedDF[["fix_time","AOI"]].copy()
-    plt.scatter(scatterDF.fix_time,scatterDF.AOI)
-    plt.show()
+    #plt.scatter(scatterDF.fix_time,scatterDF.AOI)
+    #plt.show()
     #create csv
     scatterDF.to_csv(partName + "_scatterData.csv",index = False)
     return
@@ -277,27 +224,29 @@ def createScatter(combinedDF, partName = "P_default"):
     Return:
     Creates csv called "alpscarfData.csv"
 """
-def createAlpscarf(combinedDF,partName = "P_default"):
-        #Remove the other stuff
-        alpDF = combinedDF[["fix_dur","AOI"]].copy()
+def createAlpscarf(combinedDF,colLabel = "AOI",partName = "P_default"):
+    """
+    #Remove the other stuff
+    alpDF = combinedDF[["fix_dur",colLabel]].copy()
 
-        #Rename all the AOIS to AOI_$num
-        alpDF.loc[:,"AOI"] = alpDF["AOI"].apply(lambda x: convertAOI(str(x)))
+    #Rename all the AOIS to AOI_$num
+    if(colLabel=="AOI"):
+        alpDF.loc[:,colLabel] = alpDF[colLabel].apply(lambda x: convertAOI(str(x)))
+    
 
+    #Add the participants column and rename fix_time to dwell_time
+    alpDF.loc[:,"p_name"] = [str(partName)]*len(alpDF["fix_dur"])
+    alpDF = alpDF.rename(index=str, columns={"fix_dur": "dwell_duration"})        
 
-        #Add the participants column and rename fix_time to dwell_time
-        alpDF.loc[:,"p_name"] = [str(partName)]*len(alpDF["fix_dur"])
-        alpDF = alpDF.rename(index=str, columns={"fix_dur": "dwell_duration"})        
+    #Reorder the columns
+    columnsToReorder = alpDF.columns.tolist()
+    columnsToReorder.reverse()
+    alpDF = alpDF[columnsToReorder]
 
-        #Reorder the columns
-        columnsToReorder = alpDF.columns.tolist()
-        columnsToReorder.reverse()
-        alpDF = alpDF[columnsToReorder]
-
-        #create alpScarfData
-        alpDF.to_csv(partName + "_alpscarfData.csv",index = False)
-        return
-
+    #create alpScarfData
+    alpDF.to_csv(partName + "_alpscarfData.csv",index = False)
+    return
+    """
 """
     Creates a csv of the data in the format for the radial transition graph tool ([AOIName],[FixationDuration],[Stimulus],[Participant])
 
@@ -311,6 +260,7 @@ def createAlpscarf(combinedDF,partName = "P_default"):
     Creates csv called "alpscarfData.csv"
 """
 def createRadial(combinedDF,partName = "P_default", isStimFunc = False, dictAOItoFunc = None):
+    """
     if(dictAOItoFunc != None and isStimFunc != False):
         print("Radial will create the 'Stimulus' category with the function name and will remove the whitespace AOI's")
     else:
@@ -338,6 +288,7 @@ def createRadial(combinedDF,partName = "P_default", isStimFunc = False, dictAOIt
 
     #Create radialData
     radialDF.to_csv(partName + "_radialData.csv",index=False)
+    """
 
 #AOInum must be a string
 #dictAOItoFunc must be {string : list of strings }
@@ -355,112 +306,52 @@ def findFuncName(AOINum, dictAOItoFunc):
     exit(1)
 
 
-"""
-    Create the colors csv file for the alpscarf tool 
 
-    Parameters:
-    combinedDF: pandasDF :dataframe of all of the eye tracking csv info
-    colorMatching: dictionary :dictionary of key value {hexColor: list of Aois with this color}
-    expectedOrder: list :list of the expected order column. By default it will be 1,2,3,....
-    isShaded: bool :a bool for whether or not the AOI's with the same color should be exactly the same color or different shades of that color
-        -Note: this is not yet implemented. By default they will all be the same color
-    partName: string :the name of the participant
+def createColors():
+    """
+        if(colorMatching == None):
+            print("No color csv will be created for the alpscarf tool")
+            return
+        
+        #Get the total amount of AOI's 
+        totalAOI = 0
+        for value in colorMatching.values():
+            totalAOI += len(value)
+        
+        #If there is no expected order, just make it go 1,2,3,4,5,6...
+        if(expectedOrder == None):
+            expectedOrder = list(range(1,totalAOI+1))
+        
+        #Create the AOI list
+        aoiList = list(range(-1,totalAOI-1))
 
-    Return:
-    Creates the colors csv file with the following format ([AOI],[AOI_order],[color])
-    The order of AOI is by default -1,0,1,... while the AOI_order is the expected order which is 1,2,3,... by default
-"""
-def createColors(combinedDF, colorMatching = None, expectedOrder = None, isShaded = False, partName = "P_default"):
+        colorsVect = []
+        for i in range(-1,len(aoiList)-1):
 
-    if(colorMatching == None):
-        print("No color csv will be created for the alpscarf tool")
-        return
-    
-    #Get the total amount of AOI's 
-    totalAOI = 0
-    for value in colorMatching.values():
-        totalAOI += len(value)
-    
-    #If there is no expected order, just make it go 1,2,3,4,5,6...
-    if(expectedOrder == None):
-        expectedOrder = list(range(1,totalAOI+1))
-    
-    #Create the AOI list
-    aoiList = list(range(-1,totalAOI-1))
-
-    colorsVect = []
-    for i in range(-1,len(aoiList)-1):
-
-        #Find the key in dictionary that has the number i
-        isFound = False
-        for key, value in colorMatching.items():
-            if(str(i) in value):
-                colorsVect.append(key)
-                isFound = True
-                break
-        #If it wasn't found throw and error
-        if(not isFound):
-            print("Error: Color matching contains an invalid AOI number")
-            exit(1)
-    
-    aoiList = list(map(convertAOI,aoiList))
-    colorsVect = list(map(convertColor,colorsVect))
-    
-    #Create the pandas DF and create csv from it
-    colorsDF = pd.DataFrame()
-    colorsDF["AOI"] = aoiList
-    colorsDF["AOI_order"] = expectedOrder
-    colorsDF["color"] = colorsVect
-
-    colorsDF.to_csv(partName+ "_colors.csv",index = False,quotechar = '\'')
-       
-"""
-    Creates all of the csv files for the scatter,alpscarf, and radial tranisiton tool 
-
-    Parameters:
-    jsonPath: string :path to directory that contains all of the json files
-    csvPath: string :path to directory that conatins all of the csv files
-    colorMatching: dictionary {color, list of AOI} :dictionary that contains a hex color and a list of AOI's that should be that color
-        -this is only used for if we create the colors.csv file
-    partName: string :the participants name we are creating these files for 
-
-    Return:
-    Creates all of the csvs as listed above
-"""
-def createAllCSV(jsonPath, csvPath,colorMatching, partName="P_default",dictAOItoFunc = None):
-
-    #Create the dataframe with all the csv files 
-    combinedDF = createCombinedDF(jsonPath,csvPath)
-
-    #Create the three csv files
-    createScatter(combinedDF,partName)
-    createAlpscarf(combinedDF,partName)
-    if(dictAOItoFunc == None):
-        createRadial(combinedDF,partName,False,None)
-    else:
-        createRadial(combinedDF,partName,True,dictAOItoFunc)
-
-    createColors(combinedDF,colorMatching,partName = partName)  
-
-#file must be of format "funcName,listNums"
-def readAOItoFuncFile(fileName):
-    toReturn = {}
-    with open(fileName, "r") as inFile:
-        contents = [x.strip("\n") for x in inFile.readlines()]
-        for line in contents:
-            stripped = line.replace(" ","_").split(",")
-            funcName = stripped[0]
-            AOIs = stripped[1:]
-
-            if(not funcName in toReturn.keys()):
-                toReturn[funcName] = AOIs
-            else:
-                print("Error: Multiple colors exist in" + fileName)
+            #Find the key in dictionary that has the number i
+            isFound = False
+            for key, value in colorMatching.items():
+                if(str(i) in value):
+                    colorsVect.append(key)
+                    isFound = True
+                    break
+            #If it wasn't found throw and error
+            if(not isFound):
+                print("Error: Color matching contains an invalid AOI number")
                 exit(1)
-    return(toReturn)
+        
+        aoiList = list(map(convertAOI,aoiList))
+        colorsVect = list(map(convertColor,colorsVect))
+        
+        #Create the pandas DF and create csv from it
+        colorsDF = pd.DataFrame()
+        colorsDF["AOI"] = aoiList
+        colorsDF["AOI_order"] = expectedOrder
+        colorsDF["color"] = colorsVect
 
-
-
+        colorsDF.to_csv(partName+ "_colors.csv",index = False,quotechar = '\'')
+    """
+ 
 """
     This program will create 3 or 4 csv files depending on the command line arguments passed in. Those csv files are as follows
     1) A csv file with the experiment data in the format for scatter plots ([time],[AOI])
@@ -480,45 +371,21 @@ def readAOItoFuncFile(fileName):
 """
 def main():
 
+    ABSPATH = "./619/Bug1/processed/1_1560946280495-1560946413840/post2aoi"
+    #print(getFileNames("./post2aoi","/*.csv"))
+    #print(getFileNames("./post2aoi","/*.json"))
+    listCSV = getFilePaths(ABSPATH,"/*.csv")
+    listJSON = getFilePaths(ABSPATH,"/*.json")
     
+    print(createCombinedDF(listJSON,listCSV).to_string(index=False))
+
+
+
+    """
     argc = len(sys.argv)
     argv = sys.argv
-    
-    #Check to make sure the user has the right input
-    if(argc != 4 and argc != 5 ):
-        print("Usage python3 genVisualData.py <path to jsonFiles> <path to csvFiles> <participantID> OPTIONAL<fileForColorAndFunctionMapping>")
-        exit(1)
+    """
 
-    #Create the paths as strings
-    jsonPath = str(sys.argv[1])
-    csvPath = str(sys.argv[2])
-    inName = -1
-
-    try:
-        inName = int(sys.argv[3])
-    except ValueError:
-        print("Error: Participant ID must be an integer")
-        exit(1)
-        
-    inName = "P" + str(inName)
-    colorMatching = None
-    myDictAOItoFunc = None
-
-    #If the user wants to create the colors.csv, they will pass in an ordering 
-    if(argc == 5):
-        colorMatching,myDictAOItoFunc = parseAOI_Func_Color(argv[4])
-    
-    #print(colorMatching)
-    #print(myDictAOItoFunc)
-    
-            
-    #Check if the jsonFiles path and csvFiles path exist
-    if(not os.path.isdir(jsonPath) or not os.path.isdir(csvPath)):
-        print("Error: Either " + jsonPath + " or " + csvPath + " does not exist")
-        exit(1)
-
-    #Create the csvs
-    createAllCSV(jsonPath, csvPath,colorMatching,inName,dictAOItoFunc=myDictAOItoFunc)
 
 
 
@@ -567,6 +434,4 @@ Command line options for getopt
 -c --colors (requires file): text file that contains a mapping of functions to hex colors
 -g --global : option that indicates the scarf plot data be generated based on the files rather than the 
 -i --id (optional but if used, requires int): the id of the participant 
-
-
 """
